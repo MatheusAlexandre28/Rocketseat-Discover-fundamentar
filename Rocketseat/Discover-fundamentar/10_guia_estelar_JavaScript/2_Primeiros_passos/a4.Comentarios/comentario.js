5714152e7586889e6ddbd35f251b0b51fca8c0ca
Matheus Alexandre C S <69991941+MatheusAlexandre28@users.noreply.github.com>
2022-05-04 12:02:04 -0300

    Vi ferramentas para ver o codigo JS
     Como criar e separar pastas linkando elas. Apenas crie um doc .html nele com a ajuda do emmet coloque uma exclamação e de enter o codigo vai estar feito, depois crie um doc .js no <body> no .html coloque uma tag <script> e como atributo dela um src nele coloque um ponto e barra juntos e o emmet vai te ajudar a achar a pasta.
     Vimos como usar comentarios com // que é em linha, /* depois */ para multi linha, e // depois de algum codigo.
     Fiz meu primeiro codigo com o console.log('') visualizei ele no devtools, codepen.io e no proprio html.

---
+++ Rocketseat/Discover-fundamentar/10_guia_estelar_JavaScript/2_Primeiros_passos/a4.Comentarios/comentario.js
@@ -0,0 +1,12 @@
+// Aqui eu tenho um comentario em linha
+
+/*
+    Aqui eu tenho um comentario multi linha
+
+    O codigo abaixo escreve alguma mensagem no console no devtools
+
+
+*/
+
+console.log('bem vindo ao starter') 
+console.log('nao iria ignorar')
