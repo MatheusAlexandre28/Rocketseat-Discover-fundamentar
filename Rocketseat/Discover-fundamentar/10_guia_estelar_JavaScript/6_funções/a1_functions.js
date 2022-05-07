18f1d47f39d3b28eff38540a6890c66319eeb6d9
Matheus Alexandre C S <69991941+MatheusAlexandre28@users.noreply.github.com>
2022-05-07 12:25:54 -0300

    function | anonymous, arguments | valores dentro
     functio: declaration coloque function depois nomeie ela, depois do nome coloque parenteses um espaço chaves, ex... function creatPhrases() {}. Dentro dessas chaves vamos colocar o nosso codigo, podemos executar esse codigo depois colocando apenas o nome que foi dado a funcion mais os parenteses, ex... creatPhrases().
     Parameters podemos colocar functions dentro de variaveis, apenas coloque a variavel, nomeie ela coloque um sinal de igual depois a funcition com os parenteses e chaves, ex... const sum = function() {}. Dentro dos parenteses podemos colocar parameters, neles podemos imprimilos dentro das chaves, com o console.log(), e podemos somar os valores colocando um sinal de mais, ex... function(number1, number2) {console.log(number1 + number2)}. Após podemos puxar esses valores fora do scope colocandoo o nome da variavel e depois colando valores a eles, ex sum(2, 3) neste caso como tem o mais no console.log() ele ira somar os valores.
     return fazendo a mesma coisa da aula passada vimos que caso querermos atribuir valores em outra constante fora do scope ele ira bugar o sistema, ou seja, se eu mandar imprimir a soma ja criado duas variaveis, ele vai primeiro executar o programa fora do scopo sem precisar imprimir ele na function e sim tentar imprimir ele fora, vamos terq transformar oq antes era  um console.log() em uma variavel para nao pugar ao puxarmos fora do scope e atribuir um return e dar um nome a esse return, a variavel tenq ter o mesmo nome do return, ex let total = number1 + number2. Agr se quisermos imprimir apenas coloque o console.log() dentro o texto interpolando, ou seja, com crases ``, e coloque o dolar e chaves {}, primeiro puxe a variavel depois coloque entre parenteses o nome da variavel feita fora do scope da function nesse caso o let number1 = 34, e let number2= 25, ex... console.log(`a soma é ${sum(number1, number2}`).

---
+++ Rocketseat/Discover-fundamentar/10_guia_estelar_JavaScript/6_funções/a1_functions.js
@@ -0,0 +1,20 @@
+// criar um aplicativo de frases motivacionais
+
+// declaration
+// function statement
+function creatPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciencia e persistencia')
+    console.log('Revisão é mãe do aprndezado')
+}
+
+// executar a função
+// execute, run, call, invoke (frases em ingles para chamar uma função)
+creatPhrases()
+creatPhrases()
+creatPhrases()
+
+console.log('Fim do programa')
+
+
+
