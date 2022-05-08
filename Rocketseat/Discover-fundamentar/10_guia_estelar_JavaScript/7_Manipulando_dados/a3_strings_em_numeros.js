a2f8f1cb995b98ebd80e0437f050468412ca5c9d
Matheus Alexandre C S <69991941+MatheusAlexandre28@users.noreply.github.com>
2022-05-08 13:15:53 -0300

    prototype | type conversion/coersion|string/p num
     Prototype ao criar um objeto o JS nos da uma cadeira de funções podemos usa-las como quisermos, elas foram feitas para facilitar a nossa programação, apenas uma explicação deq JS é uma linguagem baseada em prototipos ja feitos, um pré programa que usamos para o nosso favor, existem varias cadeias de prototipos e ao podemos acessa-las colocando apenas um ponto depois de um tipo de dado ex... "string".aquiVemOprototype.
     type coesion é quando o proprio JS nos ajuda a transformar um dado em algo que funcione, ex se somarmos uma string com um numero eles irao se fundir e irao concatenar. Ja o type conversion a gente mesmo que converte a string para um numero para que ela some.
     strings em numeros, apenas colocamos uma function Number() ou uma String() para transformar oq tem dentro naquilo desejado

---
+++ Rocketseat/Discover-fundamentar/10_guia_estelar_JavaScript/7_Manipulando_dados/a3_strings_em_numeros.js
@@ -0,0 +1,7 @@
+// Manipulando Strings e Numeros
+
+// Transformar String em Numero e Numero em String
+let string = "123"
+console.log(Number(string))
+let number = 321
+console.log(String(number))
