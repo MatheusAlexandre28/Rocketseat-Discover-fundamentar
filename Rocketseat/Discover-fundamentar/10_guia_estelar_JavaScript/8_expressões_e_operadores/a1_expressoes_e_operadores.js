feee3a75faab6d0b5681277d8d170edbfa801b0a
Matheus Alexandre C S <69991941+MatheusAlexandre28@users.noreply.github.com>
2022-05-09 12:00:59 -0300

    expression/operators | new | typeof/delet | ariti
     Expressions and operators, expressions  são linhas de codigos, ja os operators são operadores que colocamos para executar alguma função, temos operatos binary que precisamos de dois elementos e um operator no meio ex... 1 + 1. Ja o operator unary é com um só elemento, ex... let number = 0; ++number. e o operator ternary precisa de 3 elementos ex... false ? 'alo' : 'nada'.
     new cria um no object  na variavel que da os memos prototypes.
     typeof me mostra oq é aquele atributo que eu coloquei, ex.. typeof "ola", é uma string. Ja o delete deleta o elemento dentro do string.
     operadores aritmeticos são * multiplicação, divisão /, soma +. subtração -, resto da divisão %, incremento ++, decremento --, exponencial **.

---
+++ Rocketseat/Discover-fundamentar/10_guia_estelar_JavaScript/8_expressões_e_operadores/a1_expressoes_e_operadores.js
@@ -0,0 +1,12 @@
+/* Expressões e Operadores
+
+- Expressions
+- Operators
+    Binary // dois elementos com uma operator ex.. + | - | * | typeof
+    Unary // apenas um elemento
+    Ternary
+*/
+
+let number = 1
+
+console.log(false ? 'alo' : 'nada')
