4479415890f10880d4e1956244efacdc90acde77
Matheus Alexandre C S <69991941+MatheusAlexandre28@users.noreply.github.com>
2022-05-17 14:05:29 -0300

    terminei o exercicio de celcius e inicei array
     Fiz o fluxo alternativo onde simplesmente jogamos a formula e substituimos oq for em C para F por fim ele calcula no console.log() com a formula e transforma celsius em fareheint e ao constrario tbm.
     Achamos informaçoes dentro de array, consegui fazer 2 exercicios mas ainda estamos vendo a parte do professor

--- Rocketseat/Discover-fundamentar/10_guia_estelar_JavaScript/11_consolidando_com_exercicios/ex4_celsius_to_fahrenheit.js
+++ Rocketseat/Discover-fundamentar/10_guia_estelar_JavaScript/11_consolidando_com_exercicios/ex4_celsius_to_fahrenheit.js
@@ -34,21 +34,30 @@ function transformDegree(degree) {
     const celsiusExist = degree.toUpperCase().includes('C')
     const fahrenheitExist = degree.toUpperCase().includes('F')
 
+    //fluxo de erro
     if(!celsiusExist && !fahrenheitExist){
         throw new Error('Grau nao identificado')
     }
 
     // fluxo ideal, F -> C
     let updateDegree = Number(degree.toUpperCase().replace("F", ""));
-    let formula = (fahrenheit) => (fahrenheit - 32) * 5/9
+    let formula = fahrenheit => (fahrenheit - 32) * 5/9
     let degreeSign = 'C'
+
+    // fluxo alternativo C -> F
+    if(celsiusExist){
+        updateDegree = Number(degree.toUpperCase().replace("C", ""));
+        formula = celsius => celsius * 9/5 + 32
+        degreeSign = 'F'
+    }
 
     return formula(updateDegree) + degreeSign
 }
 
 try {
-    transformDegree('50F')
-    transformDegree('50Z')
+    console.log(transformDegree('50F'))
+    console.log(transformDegree('10C'))
+    //transformDegree('50Z')
 } catch (error) {
     console.log(error.message)
 }
---
+++ Rocketseat/Discover-fundamentar/10_guia_estelar_JavaScript/11_consolidando_com_exercicios/ex5_buscando_e_encontrando_dados_em_array.js
@@ -0,0 +1,79 @@
+/*
+    * Buscando e contando dados em Arrays *
+
+    Baseado no Array de Livros por Categoria abaixo, faça os seguintes desafios
+
+        - Contar o nummero de categorias e o numero de livros em cada categoria
+        - Contar o numero de autores
+        - Mostrar livros do autor auguto cury
+        - Transformar a função acima em uma função que irá receber o nome do autor e devolver os liros desse autor.
+*/
+
+const booksByCategory = [
+    {
+        category: "Riqueza",
+        books: [
+            {
+                title:  'Os segredos da mente milionaria',
+                author: 'T. harv eker',
+            },
+            {
+                title: "O homem mais rico da babilonia",
+                author: "George S. Clason",
+            },
+            {
+                title: "plai rico, pai pobre",
+                author: "robert T. kiyosaki e sharon Ç. lechter"
+            },
+        ],
+    },
+    {
+        category: "Inteligencia Emocional",
+        books: [
+            {
+                title: "Voce é insubstituivel",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Ansiedade - como enfrentar o mal do seculo",
+                author: "augusto cury",
+            },
+            {
+                title: "os 7 habitos das pessoas altamente eficazes",
+                author: "stephen R. coveyy",
+            },
+        ],
+    },
+];
+
+/* - meu codigo
+
+function calcCategoryAndBooks(){
+    calcCategory = booksByCategory.length
+    calcBookcategory0 = booksByCategory[0].books.length
+    calcBookcategory1 = booksByCategory[1].books.length 
+    
+    console.log(`O numero de categoria é ${calcCategory}, de livros na primeira é ${calcBookcategory0}, e na segunda é ${calcBookcategory1}`)
+}
+
+// calcCategoryAndBooks()
+
+
+function booksToAugustCury(){
+    nameAuthor = booksByCategory[1].books[0].author
+    fistbook = booksByCategory[1].books[0].title
+    segundarybook = booksByCategory[1].books[1].title
+
+    console.log(`O primeiro livro na lista de "${nameAuthor}" é o "${fistbook}" e o segundo é o "${segundarybook}"`)
+}
+
+booksToAugustCury()
+*/
+
+// Codigo professor
+
+const totalCategories = booksByCategory.length
+
+for(let category of booksByCategory){
+    console.log('total de livros da categoria: ', category.category)
+}
