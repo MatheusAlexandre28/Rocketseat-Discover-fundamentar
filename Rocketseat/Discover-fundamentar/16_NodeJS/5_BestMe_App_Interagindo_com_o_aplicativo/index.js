c822c7080c1a446be3df8ca4b9d337a56a22fe9f
Matheus Alexandre C S <69991941+MatheusAlexandre28@users.noreply.github.com>
2022-06-27 16:32:27 -0300

    Array no app
    fizemos um array fora do stdin.on e fizemos um .push neste array dentro do stdin, no caso ele vai pegar oq eu escrever e vai colocar dentro do array, depois disso fizemos um if, q pegava o tamanho deste array, e pegava o tamanho do array questions, se caso o array answers for menor q o questions ele vai rodar o if, no if colocamos para rodar novamente a ask e quando ela rodasse ia colocar o tamanho do array answers, ou seja, a cada pergunta a length aumenta, assim ele vai mudando a length do array questions tbm, pegando uma pergunta após a outra e uma resposta após a outra, depois disso queremos q o programa finalize quando acabar as questions, ent colocamos um else{} nele colocamos para imprimir o array answers q agr está com nossas respostas e finalizar o programa com o comando process.exit().

--- Rocketseat/Discover-fundamentar/16_NodeJS/5_BestMe_App_Interagindo_com_o_aplicativo/index.js
+++ Rocketseat/Discover-fundamentar/16_NodeJS/5_BestMe_App_Interagindo_com_o_aplicativo/index.js
@@ -6,12 +6,19 @@ const questions = [
 ]
 
 const ask = ( index = 0 ) => {
-  process.stdout.write(questions[index] + "\n\n\n") 
+  process.stdout.write("\n\n" + questions[index] + " > ") 
 }
 
 ask(2)
 
+const answers = []
+
 process.stdin.on("data", data => {
-  process.stdout.write(data.toString().trim() + '\n')
-  process.exit()
+  answers.push(data.toString().trim())
+  if(answers.length < questions.length) {
+    ask(answers.length)
+  }else{
+    console.log(answers)
+    process.exit()
+  }
 })
