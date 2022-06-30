e280e3b615dbeac642755972bdd5682f96389dda
Matheus Alexandre C S <69991941+MatheusAlexandre28@users.noreply.github.com>
2022-06-30 15:52:35 -0300

    objetos para ejs | foreach
     Primeiro passamos no nosso arquivo header.ejs modificamos a nossa sintaxe para passar um objeto {}, mas fizemos isso fazendo um codigo em uma linha só com a sintaxe <%= %> isso indica q ele deve renderizar isso dinamicamente, com isso passamos o typeof o nome do objeto e perguntamos q se ele for diferente de 'undefined' ele vai mostrar 'home' neste caso colocamos o nome do objeto depois do sinal de pergunta e oq ele devera aparecer na tela, com isso nao precisamos ficar colocando este objeto de pagina em pagina, e podemos apenas nomear cada pagina com o objeto 'pagina', o codigo fica assim...
     <%= %> = para mostrar q é dinamico
     <%= typeof pagina %> = mostra q este é o objeto q se deve buscar
     <%= typeof pagina != 'undefined'?%> = pergunta se pagina é diferente de undefined
     <%= typeof pagina != 'undefined'? pagina: 'Home'%> = mostra a resposta q queremos q ele passe no objeto 'pagina'.
     Assim se formos em cada arquivo da 'pages' n iremos precisar colocar o objeto 'paginas' só se quisermos, e se colocarmos podemos alterar o conteudo dele, neste caso tudo q nao estiver com o objeto 'pagina' alterando o seu conteudo ira ser mostrado 'home'.
     Tbm organizamos os arquivos em pastas uma sendo pages e a outra partials, neste caso mudamos todos os <%- include %> para modificar o caminho, usamos ../ para voltar diretorios e entrar em outros.
     Iniciamos foreach mechemos no nosso index.js colocando <ul> e varias <li> e <strong> dentro das <li>, depois fomos no nosso arquivo server.js e criamos uma variavel array []com varios objetos {}.

--- Rocketseat/Discover-fundamentar/17_NodeJS_mais_EJS/server.js
+++ Rocketseat/Discover-fundamentar/17_NodeJS_mais_EJS/server.js
@@ -3,11 +3,37 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.get("/", function(req, res){
-  res.render("index");
+  const items = [
+    {
+        title: "D",
+        message: "Desenvolver aplicações/serviços de forma facil"
+  },
+  {
+    title: "E",
+    message: "EJS usa JavaScript para renderizar HTML"
+  },
+  {
+    title: "M",
+    message: "Muito facil de usar"
+  },
+  {
+    title:"A",
+    message: "Amorzinho"
+  },
+  {
+    title: "I",
+    message: "install ejs"
+  },
+  {
+    title: "S",
+    message: "Sintaxe simples"
+  }
+  ]
+  res.render("pages/index");
 })
 
 app.get("/sobre", function(req, res){
-  res.render("about");
+  res.render("pages/about");
 })
 
 app.listen(8080);
